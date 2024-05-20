23c11d50800f2804d3a323fc9f1f84d35396fadf
Romsesito <jrom1518@gmail.com>
2024-05-20 08:53:45 -0500

    Cambios Finales del Taller

--- Notes/Views/NotePage.xaml.cs
+++ Notes/Views/NotePage.xaml.cs
@@ -1,5 +1,5 @@
 namespace Notes.Views;
-
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class NotePage : ContentPage
 {
 	public NotePage()
@@ -18,19 +18,24 @@ public partial class NotePage : ContentPage
 
     string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
 
-    private void SaveButton_Clicked(object sender, EventArgs e)
+    private async void SaveButton_Clicked(object sender, EventArgs e)
     {
-        // Save the file.
-        File.WriteAllText(_fileName, TextEditor.Text);
+        if (BindingContext is Models.Note note)
+            File.WriteAllText(note.Filename, TextEditor.Text);
+
+        await Shell.Current.GoToAsync("..");
     }
 
-    private void DeleteButton_Clicked(object sender, EventArgs e)
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
     {
-        // Delete the file.
-        if (File.Exists(_fileName))
-            File.Delete(_fileName);
+        if (BindingContext is Models.Note note)
+        {
+            // Delete the file.
+            if (File.Exists(note.Filename))
+                File.Delete(note.Filename);
+        }
 
-        TextEditor.Text = string.Empty;
+        await Shell.Current.GoToAsync("..");
     }
 
     private void LoadNote(string fileName)
@@ -46,4 +51,12 @@ public partial class NotePage : ContentPage
 
         BindingContext = noteModel;
     }
+
+
+        public string ItemId
+    {
+        set { LoadNote(value); }
+    }
+
+
 }
